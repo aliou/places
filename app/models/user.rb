60b19d80c4889a5d361dd21414fb4a5bd8657f23
Aliou Diallo <code@aliou.me>
2014-10-26 21:21:28 +0100

    Implement User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,24 @@
 class User < ActiveRecord::Base
+  validates :uid         , presence: true
+  validates :provider    , presence: true
+  validates :oauth_token , presence: true
+  validates :oauth_secret, presence: true
+
+  # Create a user from its omniauth authentification details.
+  #
+  # auth - The omniauth authentification details.
+  #
+  # Returns self.
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider     = auth['provider']
+      user.uid          = auth['uid']
+      user.oauth_token  = auth['credentials']['token']
+      user.oauth_secret = auth['credentials']['secret']
+
+      if auth['info']
+        user.name       = auth['info']['name'] || ""
+      end
+    end
+  end
 end
