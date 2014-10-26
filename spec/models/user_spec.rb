8aff134eb29ff1093372d9a707fbd2f43a175ce6
Aliou Diallo <code@aliou.me>
2014-10-26 21:13:05 +0100

    Update omniauth data mock.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,22 +2,22 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
   describe 'validations' do
-    before do
-      @omniauth_data = {
-        provider: 'foursquare',
-        uid: '0',
-        name: Faker::Name.name,
-        credentials: { token: Faker::Internet.password,
-                       secret: Faker::Internet.password }
+    let (:omniauth_data) { {
+        'provider'    => 'foursquare',
+        'uid'         => '0',
+        'name'        => Faker::Name.name,
+        'credentials' => { 'token'  => Faker::Internet.password,
+                           'secret' => Faker::Internet.password }
       }
-    end
+    }
+
     it 'should have its omniauth attributes set' do
       user = User.new
 
       expect(user).to_not be_valid
     end
     it 'should only be created using the create_with_omniauth method' do
-      user = User.create_with_omniauth(@omniauth_data)
+      user = User.create_with_omniauth(omniauth_data)
 
       expect(user).to be_valid
     end
