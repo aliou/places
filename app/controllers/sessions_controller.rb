2b3fa6bebb5c4edec386603627e8f082fea08247
Aliou Diallo <code@aliou.me>
2014-10-27 22:10:11 +0100

    Create Sessions controller.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,33 @@
+class SessionsController < ApplicationController
+
+  skip_before_filter :authenticate!
+
+  def create
+    user = User.where(provider: auth_hash['provider'], uid: auth_hash['uid'].to_s).first
+    user = User.create_with_omniauth(auth_hash) if user.nil?
+
+    reset_session
+    session[:user_id] = user.id
+    redirect_to root_url, notice: 'You have been signed in.'
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_url, notice: 'You have been signed out.'
+  end
+
+  def failure
+    redirect_to root_url,
+      notice: "An error occured: #{params[:message].humanize}."
+  end
+
+  private
+
+  # Private: Exposes the authentification Hash from Omniauth.
+  #
+  # Returns: A Hash with the Foursquare authentification details.
+  def auth_hash
+    raise auth
+    request.env['omniauth.auth']
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   root 'home#index'
+
+  # Omniauth callbacks.
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get '/auth/failure',            to: 'sessions#failure'
+
+  # User actions.
+  get '/signout',                 to: 'sessions#destroy', as: :signout
 end
