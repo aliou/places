bc5b3472d6f1700b8f16a820709150dda3cb39ca
Aliou Diallo <code@aliou.me>
2014-10-28 08:15:14 +0100

    Find or create user when necessary.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,22 @@ class User < ActiveRecord::Base
   validates :uid, inclusion: { in: [ ENV['FOURSQUARE_USER_ID'] ] ,
                                message: 'Unauthorized user.' }
 
+  # Find or create a new User depending on omniauth data.
+  #
+  # Returns the User.
+  def self.find_or_create_with_omniauth(auth)
+    if user = User.where(uid: auth['uid']).first
+      user
+    else
+      self.create_with_omniauth(auth)
+    end
+  end
+
   # Create a user from its omniauth authentification details.
   #
   # auth - The omniauth authentification details.
   #
-  # Returns self.
+  # Returns the User.
   def self.create_with_omniauth(auth)
     create do |user|
       user.provider     = auth['provider']
