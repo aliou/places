c91bffbd183be893bc32c7d9eb7317e777385af4
Aliou Diallo <code@aliou.me>
2014-10-30 10:38:56 +0100

    Test Places validations.

---
+++ spec/models/place_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Place, :type => :model do
+  describe 'validations' do
+
+    it 'has a name, latitude and longitude' do
+      place = Place.new
+
+      expect(place).to_not be_valid
+    end
+
+  end
+end
