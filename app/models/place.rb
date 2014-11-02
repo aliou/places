0c0b14f7432edc3027d648b63d44dd23d2d84d0a
Aliou Diallo <code@aliou.me>
2014-11-02 22:35:16 +0100

    Update geokit macro.

--- app/models/place.rb
+++ app/models/place.rb
@@ -20,7 +20,13 @@ class Place < ActiveRecord::Base
   # Macros                                                                     #
   ##############################################################################
 
-  acts_as_mappable :default_units => :kms
+  acts_as_mappable :default_units => :kms,
+                 :lat_column_name => :lat,
+                 :lng_column_name => :lng
+
+  ##############################################################################
+  # Class Methods                                                              #
+  ##############################################################################
 
   # Public: Finds or creates a new Place from a Foursquare venue.
   #
