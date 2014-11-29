5e9495f464806e07a4cabb3b8eb4ae2b94597e4f
Aliou Diallo <code@aliou.me>
2014-11-29 14:37:42 +0100

    Add the Place's Category in the serialized object.

--- app/serializers/place_serializer.rb
+++ app/serializers/place_serializer.rb
@@ -1,3 +1,10 @@
 class PlaceSerializer < ActiveModel::Serializer
   attributes :name, :lat, :lng, :address, :category
+
+  def category
+    {
+      name:      object.category.name,
+      icon_url:  object.category.icon_url
+    }
+  end
 end
