6d593649797e1ae0a8c3fccf0eb96e2136348449
Aliou Diallo <code@aliou.me>
2014-11-29 13:32:38 +0100

    Save category icon url in model.

--- app/models/category.rb
+++ app/models/category.rb
@@ -11,15 +11,48 @@
 #
 
 class Category < ActiveRecord::Base
+
+  ICON_TYPE = 'bg_64'
+
+  ##############################################################################
+  #                                                                            #
+  ##############################################################################
   store_accessor :metadata, :foursquare_data
+
+  ##############################################################################
+  # Associations                                                               #
+  ##############################################################################
   has_many :places
+
+  ##############################################################################
+  # Validations                                                                #
+  ##############################################################################
+
+  validates :name,     presence: true
+
+  ##############################################################################
+  # Callbacks                                                                  #
+  ##############################################################################
+
+  before_create :set_icon_url
+
+  ##############################################################################
+  # Class Methods                                                              #
+  ##############################################################################
 
   def self.find_or_create_from_foursquare_category(cat)
     category = Category.where(name: cat['name']).first
     if category.nil?
-      category = Category.create(name: cat['name'], foursquare_data: category)
+      category = Category.create(name: cat['name'], foursquare_data: cat.to_json)
     end
 
     category
+  end
+
+  private
+
+  def set_icon_url
+    data = JSON.parse(self.foursquare_data)['icon']
+    self.icon_url = data['prefix'] + Category::ICON_TYPE + data['suffix']
   end
 end
