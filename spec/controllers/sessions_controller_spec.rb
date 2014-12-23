b74037e97ac2f3dd7b70ba42caa381472ede69cf
Aliou Diallo <code@aliou.me>
2014-12-23 22:11:57 +0100

    Start spec'ing foursquare login.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe SessionsController, :type => :controller do
+RSpec.describe SessionsController do
+  describe 'GET /auth/foursquare/callback' do
+    context 'with the wrong omniauth credentials' do
 
+      before do
+        request.env['omniauth.auth'] = stub_oauth(
+          uid:   Faker::Number.number(6),
+          token: Faker::Internet.password)
+      end
+
+      it "doesn't save the user" do
+        expect { get :create, provider: 'foursquare' }.to raise_error
+      end
+    end
+  end
 end
