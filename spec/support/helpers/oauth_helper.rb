bb10b3b118d8b87126529cd33d99d96671a4af8d
Aliou Diallo <code@aliou.me>
2014-12-24 16:38:14 +0100

    Update oauth helper.

--- spec/support/helpers/oauth_helper.rb
+++ spec/support/helpers/oauth_helper.rb
@@ -2,9 +2,7 @@ module OauthHelper
   def stub_oauth(options = {})
     OmniAuth.config.add_mock(
       :foursquare,
-      auth: {
-        uid: options[:uid]
-      },
+      uid: options[:uid],
       credentials: {
         token: options[:token]
       }
