d08c2f97a995674b2378eb137cb1dff5c29dc426
Aliou Diallo <code@aliou.me>
2014-12-24 19:26:52 +0100

    Use VCR to stub requests to foursquare.

---
+++ spec/support/vcr.rb
@@ -0,0 +1,9 @@
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+
+  c.filter_sensitive_data('<FOURSQUARE_USER_ID>')    { ENV['FOURSQUARE_USER_ID'] }
+  c.filter_sensitive_data('<FOURSQUARE_USER_TOKEN>') { ENV['FOURSQUARE_USER_TOKEN'] }
+  c.filter_sensitive_data('<FOURSQUARE_KEY>')        { ENV['FOURSQUARE_KEY'] }
+  c.filter_sensitive_data('<FOURSQUARE_SECRET>')     { ENV['FOURSQUARE_SECRET'] }
+end
