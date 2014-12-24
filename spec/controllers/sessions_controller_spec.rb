e7725c18de4040293115af0ba76917912ac42722
Aliou Diallo <code@aliou.me>
2014-12-24 16:37:41 +0100

    Spec the signing out process.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -22,4 +22,16 @@ RSpec.describe SessionsController do
       end
     end
   end
+
+  describe 'GET /signout' do
+    it 'resets the session' do
+      get :destroy
+      expect(session[:user_id]).to be_nil
+    end
+
+    it 'redirects to the root path' do
+      get :destroy
+      expect(response).to redirect_to(root_path)
+    end
+  end
 end
