3f566e4b662cd6b47ef165cb49c9497f92cc4e79
Aliou Diallo <code@aliou.me>
2014-12-27 12:11:02 +0100

    Rewrite User#find_or_create_with_omniauth spec.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,24 +1,38 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-  describe 'validations' do
-    let (:omniauth_data) { {
-        'provider'    => 'foursquare',
-        'uid'         => ENV['FOURSQUARE_USER_ID'],
-        'name'        => Faker::Name.name,
-        'credentials' => { 'token'  => Faker::Internet.password }
-      }
-    }
+RSpec.describe User do
+  include ActiveJob::TestHelper
 
-    it 'should have its omniauth attributes set' do
-      user = User.new
+  it { should have_many(:places) }
+  it { should validate_presence_of :uid }
+  it { should validate_presence_of :provider }
+  it { should validate_presence_of :oauth_token }
+  it { should validate_inclusion_of(:uid).
+       in_array([ENV['FOURSQUARE_USER_ID']]) }
 
-      expect(user).to_not be_valid
-    end
-    it 'should only be created using the create_with_omniauth method' do
-      user = User.create_with_omniauth(omniauth_data)
+  describe '#find_or_create_with_omniauth' do
+    context "the user doesn't exist" do
 
-      expect(user).to be_valid
+      let(:auth) { stub_oauth(
+        uid:   ENV['FOURSQUARE_USER_ID'],
+        token: Faker::Internet.password
+      )}
+
+      before do
+        clear_enqueued_jobs
+      end
+
+      it 'creates a new user' do
+        user = User.find_or_create_with_omniauth(auth)
+
+        expect(user).to be_valid
+      end
+
+      it "imports the user's places in a job" do
+        user = User.find_or_create_with_omniauth(auth)
+
+        expect(ActiveJob::Base.queue_adapter.enqueued_jobs).to_not be_empty
+      end
     end
   end
 end
