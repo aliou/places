0989385468c3d59d6cf14907f08037a1390f9d8a
Aliou Diallo <code@aliou.me>
2015-01-17 12:15:08 +0100

    Create place factory.

---
+++ spec/factories/place.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :place do
+    name                { Faker::Name.name }
+    lat                 { Faker::Address.latitude }
+    lng                 { Faker::Address.longitude }
+    foursquare_venue_id { Faker::Internet.password }
+  end
+end
