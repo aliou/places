e066e5a3f8d7576d24638c1a36c4b8aee3d1da8f
Aliou Diallo <code@aliou.me>
2015-01-17 15:05:41 +0100

    Spec Places#index route.

--- spec/controllers/places_controller_spec.rb
+++ spec/controllers/places_controller_spec.rb
@@ -2,12 +2,30 @@ require 'rails_helper'
 
 RSpec.describe PlacesController do
   it { should route(:get,    '/places')       .to(action: :index) }
-  it { should route(:post,   '/places')       .to(action: :create) }
   it { should route(:get,    '/places/new')   .to(action: :new) }
   it { should route(:get,    '/places/1/edit').to(action: :edit, id: 1) }
   it { should route(:get,    '/places/1')     .to(action: :show, id: 1) }
+  it { should route(:post,   '/places')       .to(action: :create) }
   it { should route(:patch,  '/places/1')     .to(action: :update, id: 1) }
   it { should route(:put,    '/places/1')     .to(action: :update, id: 1) }
   it { should route(:delete, '/places/1')     .to(action: :destroy, id: 1) }
   it { should route(:get,    '/places/import').to(action: :import) }
+
+  describe 'GET .index' do
+    let(:current_user) { FactoryGirl.create(:user_with_places) }
+
+    it 'assigns the places' do
+      places = current_user.places
+      get :index, nil, user_id: current_user.id
+
+      expect(assigns(:places)).to eq(places)
+    end
+
+    it 'renders the index template' do
+      get :index, nil, user_id: current_user.id
+
+      expect(response).to render_template('index')
+    end
+  end
+
 end
