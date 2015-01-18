26dd418efbf05a66d45029cdfcf0eef99d10b1b8
Aliou Diallo <code@aliou.me>
2015-01-19 00:05:50 +0100

    Spec PlaceImportJob.perform.

--- spec/jobs/place_import_job_spec.rb
+++ spec/jobs/place_import_job_spec.rb
@@ -9,7 +9,21 @@ RSpec.describe PlaceImportJob do
     expect(ActiveJob::Base.queue_adapter.enqueued_jobs).to_not be_empty
   end
 
-  def stub_auth
-    stub_oauth(uid: ENV['FOURSQUARE_USER_ID'], token: Faker::Internet.password)
+  describe '.perform' do
+    let (:user) { FactoryGirl.create(:user) }
+
+    around do |example|
+      VCR.use_cassette('place_import_job.import') do
+        example.run
+      end
+    end
+
+    it 'imports the user places' do
+      expect { PlaceImportJob.perform_now(user) }.to change { Place.count }
+    end
   end
+end
+
+def stub_auth
+  stub_oauth(uid: ENV['FOURSQUARE_USER_ID'], token: Faker::Internet.password)
 end
