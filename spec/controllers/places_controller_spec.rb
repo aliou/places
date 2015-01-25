e344845d457f86800e4c329f98b2e91f427324c0
Aliou Diallo <code@aliou.me>
2015-01-25 18:28:50 +0100

    Test PlacesController when the user is not auth'ed.

--- spec/controllers/places_controller_spec.rb
+++ spec/controllers/places_controller_spec.rb
@@ -11,6 +11,14 @@ RSpec.describe PlacesController do
   it { should route(:delete, '/places/1')     .to(action: :destroy, id: 1) }
   it { should route(:get,    '/places/import').to(action: :import) }
 
+  context 'not authenticated' do
+    it 'redirect to the root_path' do
+      get :index
+
+      expect(response).to redirect_to(root_path)
+    end
+  end
+
   describe 'GET .index' do
     let(:current_user) { FactoryGirl.create(:user_with_places) }
 
