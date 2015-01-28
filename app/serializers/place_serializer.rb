874feb0487e89d5290082e23ce807e4cf50e4b49
Aliou Diallo <code@aliou.me>
2015-01-28 09:07:29 +0100

    Document category method in PlaceSerializer.

--- app/serializers/place_serializer.rb
+++ app/serializers/place_serializer.rb
@@ -1,10 +1,14 @@
 class PlaceSerializer < ActiveModel::Serializer
   attributes :id, :slug, :name, :lat, :lng, :address, :category
 
+  # Public: Get a simplified version of the associated category.
+  #
+  # Returns a Hash.
   def category
     {
-      name:      object.category.name,
-      icon_url:  object.category.icon_url
+      id:       object.category.id
+      name:     object.category.name,
+      icon_url: object.category.icon_url
     }
   end
 end
