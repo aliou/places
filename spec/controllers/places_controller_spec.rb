82e9f89d2f079ef83333e014e56953178a50ccec
Aliou Diallo <code@aliou.me>
2015-02-01 16:31:57 +0100

    Filter places by their distance to the origin.

--- spec/controllers/places_controller_spec.rb
+++ spec/controllers/places_controller_spec.rb
@@ -53,7 +53,8 @@ RSpec.describe PlacesController do
 
       # TODO: This could be way better.
       it 'assigns the places within 10 kms of the origin point' do
-        get :index, { format: :json, origin: origin }, user_id: current_user.id
+        get :index, { format: :json, origin: origin, zoom: 16},
+          user_id: current_user.id
 
         response_ids = response_body.map { |p| p["id"] }
         expect(response_ids).to match_array(@places.map(&:id))
