5a3f45789ee568af6639b5cb627dde01fe7630fc
Aliou Diallo <code@aliou.me>
2015-02-04 19:51:34 +0100

    Also send the Place as JSON.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -25,6 +25,11 @@ class PlacesController < ApplicationController
   # GET /places/:id
   def show
     @place = Place.find params[:id]
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @place }
+    end
   end
 
   # POST /places
