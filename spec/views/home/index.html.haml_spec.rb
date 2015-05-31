4f764d8ed732234e0aa80bae8d65d057afc474ca
Aliou Diallo <code@aliou.me>
2015-06-01 01:18:46 +0200

    Add view specs.

---
+++ spec/views/home/index.html.haml_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'home/index' do
+  it 'displays the App name' do
+    render
+    expect(rendered).to include('Places')
+  end
+
+  it 'displays the tagline' do
+    render
+    expect(rendered).to include(t('home.tagline'))
+  end
+
+  it 'displays the connection button' do
+    render
+    expect(rendered).to include('Login with Foursquare')
+  end
+end
