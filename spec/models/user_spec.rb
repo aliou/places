504f94f2c4e400f2ba58aa277182bc3bbb7e8b4b
Aliou Diallo <code@aliou.me>
2015-07-13 17:06:23 +0200

    Update rails.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,10 +7,10 @@ RSpec.describe User do
   end
 
   context 'validations' do
-    it { should validate_presence_of :provider }
-    it { should validate_presence_of :oauth_token }
-    it { should validate_presence_of :uid }
-    it { should validate_uniqueness_of :uid }
+    it { should validate_presence_of(:provider) }
+    it { should validate_presence_of(:oauth_token) }
+    it { should validate_presence_of(:uid) }
+    it { should validate_uniqueness_of(:uid) }
     it { should validate_inclusion_of(:uid).in_array(authorized_uids) }
   end
 
